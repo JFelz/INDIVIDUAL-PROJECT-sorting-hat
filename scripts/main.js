6ab2e27550932ceeefbef08ade0dbb96634a4258
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-01-30 18:33:14 -0600

    Adding classes info to Array

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,22 @@
 import { renderToDom } from "../utils/renderToDom.js";
 
-let aBs = 10;
+renderToDom();
 
-renderToDom(".header", aBs);
+//input button that creates a new array with user info
+
+const createMember = () =>{
+const btnName = document.querySelector("#Name");
+
+const newMember = {
+id: 1,
+name: btnName.value,
+}
+}
+
+
+
+//display user info on card and place it in respective order.
+
+
+//create form
+//1.) target user input box and store info
