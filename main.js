71355ebd1bcff844ac54dce3799a2ad83578c420
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-01-31 14:06:08 -0600

    Removed folders. All info in JS folde nowr

--- main.js
+++ main.js
@@ -1,10 +1,393 @@
-import { renderToDom } from "../utils/renderToDom.js";
-import { GoTclasses } from "../data/classes.js";
-import { CardsOnDom } from "../components/card.js";
+const GoTclasses = [
+  {
+  id: 1,
+  name: "Marley",
+  faction: "Lannister",
+  class: "Assassin",
+  weapon: "Dual Daggers",
+  specialAbility: "Everybody can see you when in stealth",
+  imgUrl: "https://i.pinimg.com/originals/9a/a1/5a/9aa15a290659bcd4aa213fdefd9d47a3.jpg"
+  },
+  {
+  id: 2,
+  name: "Cerelia",
+  faction: "Lannister",
+  class: "Knight",
+  weapon: "Dual-Handed GreatSword",
+  specAbility: "You struggle to carry your sword.",
+  imgUrl: "https://i.pinimg.com/564x/ac/0a/ce/ac0acee0c2863ff70a4b21cb74281d38.jpg"
+  },
+  {
+  id: 3,
+  name: "Miranda",
+  faction: "Lannister",
+  class: "Sorcerer",
+  weapon: "Power-wielding hands",
+  specAbility: "Everytime you cast a spell you hurt your health.",
+  imgUrl: "https://cdna.artstation.com/p/assets/images/images/011/575/376/large/zhu-liu-0629.jpg?1530280138"
+  },
+  {
+  id: 4,
+  name: "Theon",
+  faction: "Lannister",
+  class: "Archer",
+  weapon: "Greatbow",
+  specAbility: "Your aim is better when you're drunk.",
+  imgUrl: "https://i.pinimg.com/originals/45/1d/8f/451d8fdb7ec8ae87e46d05b3356d399e.jpg"
+  },
+  {
+  id: 5,
+  name: "Alaric",
+  faction: "Lannister",
+  class: "Bard",
+  weapon: "A Mandolin and a cup of Mead",
+  specAbility: "Really good at pissing people off.",
+  imgUrl: "https://static.wikia.nocookie.net/witcher/images/2/2c/Gwent_cardart_scoiatael_eldain.png/revision/latest?cb=20181022104739"
+  },
+  {
+  id: 6,
+  name: "Burchard",
+  faction: "Lannister",
+  class: "Infantry",
+  weapon: "Sword and Shield",
+  specAbility: "Most likely to die first.",
+  imgUrl: "https://cdnb.artstation.com/p/assets/images/images/007/853/071/large/alex-vasin-2view.jpg?1508936330"
+  },
+  {
+  id: 7,
+  name: "Cassian",
+  faction: "Lannister",
+  class: "Scholar",
+  weapon: "Scrolls and Books",
+  specAbility: "You're blind: You can only read books and scrolls in braille.",
+  imgUrl: "https://static.wikia.nocookie.net/oustomiaworld/images/0/0c/Claus.jpeg/revision/latest?cb=20161210114955"
+  },
+  {
+  id: 8,
+  name: "Arne",
+  faction: "Stark",
+  class: "Assassin",
+  weapon: "Dual Daggers",
+  specialAbility: "Everybody can see you when in stealth",
+  imgUrl: "https://i.pinimg.com/originals/9a/a1/5a/9aa15a290659bcd4aa213fdefd9d47a3.jpg"
+  },
+  {
+  id: 9,
+  name: "Gomes",
+  faction: "Stark",
+  class: "Knight",
+  weapon: "Dual-Handed GreatSword",
+  specAbility: "You struggle to carry your sword.",
+  imgUrl: "https://i.pinimg.com/564x/ac/0a/ce/ac0acee0c2863ff70a4b21cb74281d38.jpg"
+  },
+  {
+  id: 10,
+  name: "Hamlin",
+  faction: "Stark",
+  class: "Sorcerer",
+  weapon: "Power-wielding hands",
+  specAbility: "Everytime you cast a spell you hurt your health.",
+  imgUrl: "https://cdna.artstation.com/p/assets/images/images/011/575/376/large/zhu-liu-0629.jpg?1530280138"
+  },
+  {
+  id: 11,
+  name: btnName.value,
+  faction: "Stark",
+  class: "Archer",
+  weapon: "Greatbow",
+  specAbility: "Your aim is better when you're drunk.",
+  imgUrl: "https://i.pinimg.com/originals/45/1d/8f/451d8fdb7ec8ae87e46d05b3356d399e.jpg"
+  },
+  {
+  id: 12,
+  name: "Lancaster",
+  faction: "Stark",
+  class: "Bard",
+  weapon: "A Mandolin and a cup of Mead",
+  specAbility: "Really good at pissing people off.",
+  imgUrl: "https://static.wikia.nocookie.net/witcher/images/2/2c/Gwent_cardart_scoiatael_eldain.png/revision/latest?cb=20181022104739"
+  },
+  {
+  id: 13,
+  name: "Osric",
+  faction: "Stark",
+  class: "Infantry",
+  weapon: "Sword and Shield",
+  specAbility: "Most likely to die first.",
+  imgUrl: "https://cdnb.artstation.com/p/assets/images/images/007/853/071/large/alex-vasin-2view.jpg?1508936330"
+  },
+  {
+  id: 14,
+  name: "Njal",
+  faction: "Stark",
+  class: "Scholar",
+  weapon: "Scrolls and Books",
+  specAbility: "You're blind: You can only read books and scrolls in braille.",
+  imgUrl: "https://static.wikia.nocookie.net/oustomiaworld/images/0/0c/Claus.jpeg/revision/latest?cb=20161210114955"
+  },
+  {
+  id: 15,
+  name: "Maegon",
+  faction: "Targaryen",
+  class: "Assassin",
+  weapon: "Dual Daggers",
+  specialAbility: "Everybody can see you when in stealth",
+  imgUrl: "https://i.pinimg.com/originals/9a/a1/5a/9aa15a290659bcd4aa213fdefd9d47a3.jpg"
+  },
+  {
+  id: 16,
+  name: "Rhaenys",
+  faction: "Targaryen",
+  class: "Knight",
+  weapon: "Dual-Handed GreatSword",
+  specAbility: "You struggle to carry your sword.",
+  imgUrl: "https://i.pinimg.com/564x/ac/0a/ce/ac0acee0c2863ff70a4b21cb74281d38.jpg"
+  },
+  {
+  id: 17,
+  name: "Traegon",
+  faction: "Targaryen",
+  class: "Sorcerer",
+  weapon: "Power-wielding hands",
+  specAbility: "Everytime you cast a spell you hurt your health.",
+  imgUrl: "https://cdna.artstation.com/p/assets/images/images/011/575/376/large/zhu-liu-0629.jpg?1530280138"
+  },
+  {
+  id: 18,
+  name: "Baelor",
+  faction: "Targaryen",
+  class: "Archer",
+  weapon: "Greatbow",
+  specAbility: "Your aim is better when you're drunk.",
+  imgUrl: "https://i.pinimg.com/originals/45/1d/8f/451d8fdb7ec8ae87e46d05b3356d399e.jpg"
+  },
+  {
+  id: 19,
+  name: "Daerion",
+  faction: "Targaryen",
+  class: "Bard",
+  weapon: "A Mandolin and a cup of Mead",
+  specAbility: "Really good at pissing people off.",
+  imgUrl: "https://static.wikia.nocookie.net/witcher/images/2/2c/Gwent_cardart_scoiatael_eldain.png/revision/latest?cb=20181022104739"
+  },
+  {
+  id: 20,
+  name: "Aenar",
+  faction: "Targaryen",
+  class: "Infantry",
+  weapon: "Sword and Shield",
+  specAbility: "Most likely to die first.",
+  imgUrl: "https://cdnb.artstation.com/p/assets/images/images/007/853/071/large/alex-vasin-2view.jpg?1508936330"
+  },
+  {
+  id: 21,
+  name: "Vyser",
+  faction: "Targaryen",
+  class: "Scholar",
+  weapon: "Scrolls and Books",
+  specAbility: "You're blind: You can only read books and scrolls in braille.",
+  imgUrl: "https://static.wikia.nocookie.net/oustomiaworld/images/0/0c/Claus.jpeg/revision/latest?cb=20161210114955"
+  },
+  {
+  id: 22,
+  name: "Giserra",
+  faction: "Baratheon",
+  class: "Assassin",
+  weapon: "Dual Daggers",
+  specialAbility: "Everybody can see you when in stealth",
+  imgUrl: "https://i.pinimg.com/originals/9a/a1/5a/9aa15a290659bcd4aa213fdefd9d47a3.jpg"
+  },
+  {
+  id: 23,
+  name: "Lex",
+  faction: "Baratheon",
+  class: "Knight",
+  weapon: "Dual-Handed GreatSword",
+  specAbility: "You struggle to carry your sword.",
+  imgUrl: "https://i.pinimg.com/564x/ac/0a/ce/ac0acee0c2863ff70a4b21cb74281d38.jpg"
+  },
+  {
+  id: 24,
+  name: "Theo",
+  faction: "Baratheon",
+  class: "Sorcerer",
+  weapon: "Power-wielding hands",
+  specAbility: "Everytime you cast a spell you hurt your health.",
+  imgUrl: "https://cdna.artstation.com/p/assets/images/images/011/575/376/large/zhu-liu-0629.jpg?1530280138"
+  },
+  {
+  id: 25,
+  name: "Tommen",
+  faction: "Baratheon",
+  class: "Archer",
+  weapon: "Greatbow",
+  specAbility: "Your aim is better when you're drunk.",
+  imgUrl: "https://i.pinimg.com/originals/45/1d/8f/451d8fdb7ec8ae87e46d05b3356d399e.jpg"
+  },
+  {
+  id: 26,
+  name: "Meera",
+  faction: "Baratheon",
+  class: "Bard",
+  weapon: "A Mandolin and a cup of Mead",
+  specAbility: "Really good at pissing people off.",
+  imgUrl: "https://static.wikia.nocookie.net/witcher/images/2/2c/Gwent_cardart_scoiatael_eldain.png/revision/latest?cb=20181022104739"
+  },
+  {
+  id: 27,
+  name: "Martel",
+  faction: "Baratheon",
+  class: "Infantry",
+  weapon: "Sword and Shield",
+  specAbility: "Most likely to die first.",
+  imgUrl: "https://cdnb.artstation.com/p/assets/images/images/007/853/071/large/alex-vasin-2view.jpg?1508936330"
+  },
+  {
+  id: 28,
+  name: "Marshall",
+  faction: "Baratheon",
+  class: "Scholar",
+  weapon: "Scrolls and Books",
+  specAbility: "You're blind: You can only read books and scrolls in braille.",
+  imgUrl: "https://static.wikia.nocookie.net/oustomiaworld/images/0/0c/Claus.jpeg/revision/latest?cb=20161210114955"
+  },
+  {
+  id: 29,
+  name: "Kaelyn",
+  faction: "Greyjoy",
+  class: "Assassin",
+  weapon: "Dual Daggers",
+  specialAbility: "Everybody can see you when in stealth",
+  imgUrl: "https://i.pinimg.com/originals/9a/a1/5a/9aa15a290659bcd4aa213fdefd9d47a3.jpg"
+  },
+  {
+  id: 30,
+  name: "Leofric",
+  faction: "Greyjoy",
+  class: "Knight",
+  weapon: "Dual-Handed GreatSword",
+  specAbility: "You struggle to carry your sword.",
+  imgUrl: "https://i.pinimg.com/564x/ac/0a/ce/ac0acee0c2863ff70a4b21cb74281d38.jpg"
+  },
+  {
+  id: 31,
+  name: "Arden",
+  faction: "Greyjoy",
+  class: "Sorcerer",
+  weapon: "Power-wielding hands",
+  specAbility: "Everytime you cast a spell you hurt your health.",
+  imgUrl: "https://cdna.artstation.com/p/assets/images/images/011/575/376/large/zhu-liu-0629.jpg?1530280138"
+  },
+  {
+  id: 32,
+  name: "Ealdread",
+  faction: "Greyjoy",
+  class: "Archer",
+  weapon: "Greatbow",
+  specAbility: "Your aim is better when you're drunk.",
+  imgUrl: "https://i.pinimg.com/originals/45/1d/8f/451d8fdb7ec8ae87e46d05b3356d399e.jpg"
+  },
+  {
+  id: 33,
+  name: "Valora",
+  faction: "Greyjoy",
+  class: "Bard",
+  weapon: "A Mandolin and a cup of Mead",
+  specAbility: "Really good at pissing people off.",
+  imgUrl: "https://static.wikia.nocookie.net/witcher/images/2/2c/Gwent_cardart_scoiatael_eldain.png/revision/latest?cb=20181022104739"
+  },
+  {
+  id: 34,
+  name: "Eadda",
+  faction: "Greyjoy",
+  class: "Infantry",
+  weapon: "Sword and Shield",
+  specAbility: "Most likely to die first.",
+  imgUrl: "https://cdnb.artstation.com/p/assets/images/images/007/853/071/large/alex-vasin-2view.jpg?1508936330"
+  },
+  {
+  id: 35,
+  name: "Lilyana",
+  faction: "Greyjoy",
+  class: "Scholar",
+  weapon: "Scrolls and Books",
+  specAbility: "You're blind: You can only read books and scrolls in braille.",
+  imgUrl: "https://static.wikia.nocookie.net/oustomiaworld/images/0/0c/Claus.jpeg/revision/latest?cb=20161210114955"
+  },
+  {
+  id: 36,
+  name: "Dena",
+  faction: "Hightower",
+  class: "Assassin",
+  weapon: "Dual Daggers",
+  specialAbility: "Everybody can see you when in stealth",
+  imgUrl: "https://i.pinimg.com/originals/9a/a1/5a/9aa15a290659bcd4aa213fdefd9d47a3.jpg"
+  },
+  {
+  id: 37,
+  name: "Aldora",
+  faction: "Hightower",
+  class: "Knight",
+  weapon: "Dual-Handed GreatSword",
+  specAbility: "You struggle to carry your sword.",
+  imgUrl: "https://i.pinimg.com/564x/ac/0a/ce/ac0acee0c2863ff70a4b21cb74281d38.jpg"
+  },
+  {
+  id: 38,
+  name: "Edrea",
+  faction: "Hightower",
+  class: "Sorcerer",
+  weapon: "Power-wielding hands",
+  specAbility: "Everytime you cast a spell you hurt your health.",
+  imgUrl: "https://cdna.artstation.com/p/assets/images/images/011/575/376/large/zhu-liu-0629.jpg?1530280138"
+  },
+  {
+  id: 39,
+  name: "Esmond",
+  faction: "Hightower",
+  class: "Archer",
+  weapon: "Greatbow",
+  specAbility: "Your aim is better when you're drunk.",
+  imgUrl: "https://i.pinimg.com/originals/45/1d/8f/451d8fdb7ec8ae87e46d05b3356d399e.jpg"
+  },
+  {
+  id: 40,
+  name: "Osgar",
+  faction: "Hightower",
+  class: "Bard",
+  weapon: "A Mandolin and a cup of Mead",
+  specAbility: "Really good at pissing people off.",
+  imgUrl: "https://static.wikia.nocookie.net/witcher/images/2/2c/Gwent_cardart_scoiatael_eldain.png/revision/latest?cb=20181022104739"
+  },
+  {
+  id: 41,
+  name: "Hildred",
+  faction: "Hightower",
+  class: "Infantry",
+  weapon: "Sword and Shield",
+  specAbility: "Most likely to die first.",
+  imgUrl: "https://cdnb.artstation.com/p/assets/images/images/007/853/071/large/alex-vasin-2view.jpg?1508936330"
+  },
+  {
+  id: 42,
+  name: "Eoforhild",
+  faction: "Hightower",
+  class: "Scholar",
+  weapon: "Scrolls and Books",
+  specAbility: "You're blind: You can only read books and scrolls in braille.",
+  imgUrl: "https://static.wikia.nocookie.net/oustomiaworld/images/0/0c/Claus.jpeg/revision/latest?cb=20161210114955"
+  },
+  ];
+  
 
 
 
 
+
+const renderToDom = (selectId, html) => {
+  const divId = document.querySelector(selectId);
+  divId.innerHTML = html;
+  };
 
 //input button that creates a new array with user info
 
@@ -21,18 +404,29 @@ import { CardsOnDom } from "../components/card.js";
 
 //display user info on card and place it in respective order.
 
-const renderCards = () => {
-let refCards = "";
+const CardsOnDom = (GoTclasses) => {
+  let domString = "";
+  
+  for (const persona of GoTclasses) {
+    domString += 
 
-GoTclasses.forEach((persona) => {
-  refCards += CardsOnDom(persona);
-});
-
-renderToDom(".blue-card", refCards);
-
-}
-
-renderCards(GoTclasses);
+  `<div class="card" style="width: 18rem;">
+  <img src="${persona.imgUrl}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">${persona.name}</p>
+    <p class="card-text">${persona.faction}</p>
+    <p class="card-text">${persona.class}</p>
+    <p class="card-text">${persona.weapon}</p>
+    <p class="card-text">${persona.specAbility}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>`
+  }
+  
+  renderToDom(".blue-card", domString);
+  
+  };
 
 //create form
 //1.) target user input box and store info
