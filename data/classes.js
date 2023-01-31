535397e2fde2ad8a542f4cd73c8dd2a0433e0913
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-01-30 19:42:02 -0600

    Finished adding all the objects within array

---
+++ data/classes.js
@@ -0,0 +1,47 @@
+const classes = [
+{
+id: 1,
+faction: ,
+class: "Assassin",
+weapon: "Dual Daggers",
+specialAbility: "Can use the masks of The Many-Faced God."
+},
+{
+id: ,
+faction: ,
+class: "Knight",
+weapon: "Dual-Handed GreatSword",
+specAbility: "Your sword is made of Valyrian Steel"
+},
+{
+id: ,
+faction: ,
+class: "Sorcerer",
+weapon: "Power-wielding hands",
+specAbility: "Can self heal."
+},
+{
+id: ,
+faction: ,
+class: "Archer",
+weapon: "Greatbow",
+specAbility: "Can fire arrows from ground level to the top of the Wall."
+},
+{
+id: ,
+faction: ,
+class: "Bard",
+weapon: "A Mandolin and a cup of Mead",
+specAbility: "Really good at pissing people off."
+},
+{
+id: ,
+faction: ,
+class: "Archer",
+weapon: "Greatbow",
+  pecAbility: "Can fire arrows from ground level to the top of the Wall."
+  },
+
+
+
+]
